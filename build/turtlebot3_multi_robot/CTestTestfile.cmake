71db6bf4f9032af4954d3b7ff2da84eb3fcb0679
joca2511 <143613939+joca2511@users.noreply.github.com>
2025-05-20 19:01:43 -0300

    Delete build directory

--- build/turtlebot3_multi_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rodrigo/TCC/src/turtlebot3_multi_robot
-# Build directory: /home/rodrigo/TCC/build/turtlebot3_multi_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
