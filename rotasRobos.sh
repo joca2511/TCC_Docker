7a6a7dacd75917635001843b2735d7b16efe837d
joca2511 <simoesruy@gmail.com>
2025-08-16 15:24:20 -0300

    Adicionando Visualizer e terminando shell

--- rotasRobos.sh
+++ rotasRobos.sh
@@ -4,7 +4,9 @@ gnome-terminal --tab --title="Terminal 1" --command="bash -c 'ros2 launch TCC tu
 
 gnome-terminal --tab --title="Terminal 2" --command="bash -c 'ros2 launch TCC multi_robot_Arena.launch.py; $SHELL'" & 
 
-read -p
+sleep 10
+
+
 
 gnome-terminal --tab --title="Terminal 3" --command="bash -c 'ros2 run TCC turtlebot3_absolute_move_Arena TB3_1 1.3 2.5 180 0.15 2.5 0; 
 $SHELL'" &
@@ -14,4 +16,4 @@ gnome-terminal --tab --title="Terminal 4" --command="bash -c 'ros2 run TCC turtl
 
 gnome-terminal --tab --title="Terminal 5" --command="bash -c 'ros2 run TCC turtlebot3_absolute_move_Arena TB3_3 3.8 2.5 180 2.5 2.5 0; $SHELL'" &
 
-
+gnome-terminal --tab --title="Terminal 6" --command="bash -c 'ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=./maps/mapaBase.yaml; $SHELL'" & 
