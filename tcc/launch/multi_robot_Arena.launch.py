c1e57acdd4a1e71f32c60fc7433b9d916c061656
joca2511 <simoesruy@gmail.com>
2025-08-31 12:55:11 -0300

    launch editado para não mais abrir 2 janelas gazebo, novos scripts de teste

--- tcc/launch/multi_robot_Arena.launch.py
+++ tcc/launch/multi_robot_Arena.launch.py
@@ -54,24 +54,10 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    world = os.path.join(
-        get_package_share_directory('tcc'),
-        'worlds',
-        'Arena.world'
-    )
+    
 
-    gzserver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        ),
-        launch_arguments={'world': world}.items()
-    )
+    
 
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
-    )
 
     robot_state_publisher_cmd_list = []
 
@@ -121,8 +107,6 @@ def generate_launch_description():
 
     ld = LaunchDescription()
     # Add the commands to the launch description
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
     ld.add_action(RegisterEventHandler(
         OnShutdown(
             on_shutdown=lambda event,
